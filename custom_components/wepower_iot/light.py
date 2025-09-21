9179858890a943653a9bed04949ec96ffda8e2a8
Irtaza Ahsan <105520819+irtizzaaa@users.noreply.github.com>
2025-09-21 19:49:52 +0300

    Update light.py

--- custom_components/wepower_iot/light.py
+++ custom_components/wepower_iot/light.py
@@ -10,7 +10,6 @@ from homeassistant.components.light import (
     ColorMode,
     ATTR_BRIGHTNESS,
     ATTR_RGB_COLOR,
-    ATTR_COLOR_TEMP,
     ATTR_COLOR_TEMP_KELVIN,
     ATTR_TRANSITION,
 )
@@ -192,8 +191,8 @@ class WePowerIoTLight(LightEntity):
                 turn_on_message["color_temp"] = color_temp_mireds
                 self._attr_color_temp = color_temp_mireds
                 self._attr_color_mode = ColorMode.COLOR_TEMP
-            elif ATTR_COLOR_TEMP in kwargs:  # Fallback for backward compatibility
-                color_temp = kwargs[ATTR_COLOR_TEMP]
+            elif "color_temp" in kwargs:  # Fallback for backward compatibility
+                color_temp = kwargs["color_temp"]
                 turn_on_message["color_temp"] = color_temp
                 self._attr_color_temp = color_temp
                 self._attr_color_mode = ColorMode.COLOR_TEMP
