e00b7f40dd0aa847e1d3fed02bf52a2f3c569036
Irtaza Ahsan <105520819+irtizzaaa@users.noreply.github.com>
2025-09-29 22:23:10 +0300

    Update const.py

--- custom_components/wepower_iot/const.py
+++ custom_components/wepower_iot/const.py
@@ -51,7 +51,7 @@ BLE_DISCOVERY_MODE_V1_AUTO: Final = "v1_auto"
 
 # Integration name and version
 INTEGRATION_NAME: Final = "WePower IoT"
-INTEGRATION_VERSION: Final = "1.0.36"
+INTEGRATION_VERSION: Final = "1.0.37"
 
 # BLE Packet Format Constants
 BLE_COMPANY_ID: Final = 0x5750  # WePower company ID
@@ -72,6 +72,7 @@ SENSOR_TYPE_PRESSURE: Final = 3
 # Signals
 SIGNAL_DEVICE_UPDATED: Final = f"{DOMAIN}_device_updated"
 SIGNAL_DEVICE_ADDED: Final = f"{DOMAIN}_device_added"
+
 
 
 
