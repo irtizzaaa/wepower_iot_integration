48531eac17459ace8bdc622c798e9c16eeef7dd6
Irtaza Ahsan <105520819+irtizzaaa@users.noreply.github.com>
2025-10-06 23:31:00 +0300

    Update const.py

--- custom_components/wepower_iot/const.py
+++ custom_components/wepower_iot/const.py
@@ -51,7 +51,7 @@ BLE_DISCOVERY_MODE_V1_AUTO: Final = "v1_auto"
 
 # Integration name and version
 INTEGRATION_NAME: Final = "Gemns™ IoT"
-INTEGRATION_VERSION: Final = "1.0.108"
+INTEGRATION_VERSION: Final = "1.0.109"
 
 # BLE Packet Format Constants
 BLE_COMPANY_ID: Final = 0x5750  # Gemns™ IoT company ID
@@ -81,4 +81,5 @@ SWITCH_TYPE_TOGGLE: Final = 9
 # Signals
 SIGNAL_DEVICE_UPDATED: Final = f"{DOMAIN}_device_updated"
 SIGNAL_DEVICE_ADDED: Final = f"{DOMAIN}_device_added"
+
 
